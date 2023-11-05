53c14714952e7ddc61edce97793d2b97228e9c3e
mehedi1256 <mehedihassanshovospirit@gmail.com>
2023-11-05 19:02:31 +0600

    updated at updated route

---
+++ app/Models/Chirp.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Chirp extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'message',
+    ];
+
+    /**
+     * this model belong to User model
+     *
+     * @return BelongsTo
+     */
+    
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
